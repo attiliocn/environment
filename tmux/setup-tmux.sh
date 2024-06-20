5477aea77aef7fad662aab14d871b8081c71acd6
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-06-20 08:59:31 -0300

    updated tmux sessions creator

--- tmux/setup-tmux.sh
+++ tmux/setup-tmux.sh
@@ -2,6 +2,7 @@
 
 function setup-tmux() {
     tmux new-session -d -s docs -c $HOME/projects/docs "nvim registro\ de\ atividades.md"
+    tmux new-session -d -s dev
     tmux new-session -d -s ssh
     tmux new-session -d -s exchange -c $HOME/projects/ufscar/ufscar.schrock-exchange
     tmux new-session -d -s metathesis -c $HOME/projects/ufscar/ufscar.schrock-metathesis
