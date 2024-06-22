d4a79eec2add9c9516928b80a263fd879515989a
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-06-22 10:09:54 -0300

    change documentation dir

--- tmux/setup-tmux.sh
+++ tmux/setup-tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function setup-tmux() {
-    tmux new-session -d -s docs -c $HOME/projects/docs "nvim registro\ de\ atividades.md"
+    tmux new-session -d -s docs -c $HOME/md "nvim registro\ de\ atividades.md"
     tmux new-session -d -s dev
     tmux new-session -d -s ssh
     tmux new-session -d -s exchange -c $HOME/projects/ufscar/ufscar.schrock-exchange
