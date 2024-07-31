5082ff7834402f62a13b231a0fe4cd28eb70acf3
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-07-31 09:50:29 -0300

    rename tmux sessions

--- tmux/setup-tmux.sh
+++ tmux/setup-tmux.sh
@@ -4,8 +4,8 @@ function setup-tmux() {
     tmux new-session -d -s docs -c $HOME/md "nvim registro\ de\ atividades.md"
     tmux new-session -d -s dev
     tmux new-session -d -s ssh
-    tmux new-session -d -s exchange -c $HOME/projects/ufscar/ufscar.schrock-exchange
-    tmux new-session -d -s metathesis -c $HOME/projects/ufscar/ufscar.schrock-metathesis
+    tmux new-session -d -s mox -c $HOME/projects/ufscar/ufscar.schrock-exchange
+    tmux new-session -d -s meta -c $HOME/projects/ufscar/ufscar.schrock-metathesis
     tmux attach-session -t docs
 }
 
