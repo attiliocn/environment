6b4420fde957f77817aa4b74437f41916a8bf175
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-08-29 08:40:48 -0300

    add new windows in setup-tmux

--- tmux/setup-tmux.sh
+++ tmux/setup-tmux.sh
@@ -2,8 +2,10 @@
 
 function setup-tmux() {
     tmux new-session -d -s docs -c $HOME/md "nvim registro\ de\ atividades.md"
+    tmux new-session -d -s bkp
     tmux new-session -d -s dev
     tmux new-session -d -s ssh
+    tmux new-session -d -s slib -c $HOME/projects/ufscar/ufscar.schrock-lib
     tmux new-session -d -s mox -c $HOME/projects/ufscar/ufscar.schrock-exchange
     tmux new-session -d -s vx -c $HOME/projects/ufscar/ufscar.vanadium
     tmux new-session -d -s meta -c $HOME/projects/ufscar/ufscar.schrock-metathesis
