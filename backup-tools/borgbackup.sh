54efd94b300f823a69184677c21e25ddfa89bc56
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2026-03-11 12:21:50 -0300

    fix: add remote and local options to borgbackup

--- backup-tools/borgbackup.sh
+++ backup-tools/borgbackup.sh
@@ -2,18 +2,21 @@
 
 timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
 
+
+
 # Define backup directory and borg repository paths
 BORG_DIR=$1
 TARGET_DIR=$2
+BKP_MODE=$3
+
+# If no mode is provided, default to "local"
+if [ -z "$BKP_MODE" ]; then
+    BKP_MODE="local"
+fi
 
 # Define logfile path
 logfile=/tmp/borg-create_"$timestamp".log
 
-# Check if BORG_DIR exist
-if [ ! -d $BORG_DIR ]; then
-  echo "BORG dir does not exist"
-  exit
-fi
 
 # Check if TARGET_DIR exist and is not empty
 if [ -d "$TARGET_DIR" ]; then
@@ -25,10 +28,20 @@ else
     echo "Directory '$TARGET_DIR' does not exist."
     exit
 fi
-
-# Get realpaths for bkp dir and borg dir
-borg_dir=$(realpath "$BORG_DIR")
 target_dir=$(realpath "$TARGET_DIR")
+
+# Set the BORG_DIR variable based on the backup mode
+if [ "$BKP_MODE" == "local" ]; then
+    # Check if BORG_DIR exist
+    if [ ! -d "$BORG_DIR" ]; then
+        echo "BORG dir does not exist"
+        exit
+    fi
+    # Get realpath
+    borg_dir=$(realpath "$BORG_DIR")
+elif [ "$BKP_MODE" == "remote" ]; then
+    borg_dir="$BORG_DIR"
+fi
 
 # Run borg create
 cd "$target_dir"
